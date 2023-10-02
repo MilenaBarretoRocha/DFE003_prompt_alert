d9704f0a07f49d9a04e68563c60613375ad08260
Milena Barreto Rocha <jcmilenasantana@gmail.com>
2023-10-02 17:51:54 -0300

    Subindo alterações

---
+++ src/main.js
@@ -0,0 +1,17 @@
+// const numero = prompt("Diga um número:")
+// const dobro = numero*2
+// alert(dobro)
+
+
+
+const jantar = prompt("Valor do jantar")
+const adicional = jantar*0.1
+const valorFinal = jantar + adicional
+alert(valorFinal)
+
+
+
+// const total = prompt("Valor da conta?")
+// const participantes = prompt("Número de participantes?")
+// const valorIndividual = (total/participantes)
+// alert (valorIndividual)
